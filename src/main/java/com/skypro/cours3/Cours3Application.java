4aed69eb56830a3ec305a164631d78998d6bc3e2
dima-yarakhovich <dima020490@tut.by>
2023-01-28 14:26:45 +0300

    Initial commit

---
+++ src/main/java/com/skypro/cours3/Cours3Application.java
@@ -0,0 +1,13 @@
+package com.skypro.cours3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Cours3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Cours3Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/skypro/cours3/Cours3ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.skypro.cours3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Cours3ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
